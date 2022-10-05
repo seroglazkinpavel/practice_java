d065545ecbd7a4dd08b75fdb975aedb43d9ade2c
PavelSeroglazlin <mr.seroglazkin@bk.ru>
2022-10-06 01:27:25 +0400

    Исправлено task_3.java

--- practice_1/task_3.java
+++ practice_1/task_3.java
@@ -6,36 +6,17 @@ import java.util.Arrays;
 
 public class task_3 {
     public static void main(String args[]) {
-        /*
-         * 1 Способ
-         * int[] array = { 3, 2, 2, 3 };
-         * System.out.println(Arrays.toString(array));
-         * int tmp = array[0];
-         * for (int i = 0; i < array.length - 1; ++i) {
-         * array[i] = array[i + 1];
-         * }
-         * array[array.length - 1] = tmp;
-         * System.out.println(Arrays.toString(array));
-         */
         int[] array = { 3, 2, 2, 3 };
-        System.out.println(Arrays.toString(array));
-        boolean needIteration = true;
-        while (needIteration) {
-            needIteration = false;
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[i - 1]) {
-                    swap(array, i, i - 1);
-                    needIteration = true;
-                }
+        int[] arr = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arr[i] = 3;
+        }
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != 3) {
+                arr[i - 1] = array[i];
             }
         }
-        System.out.println(Arrays.toString(array));
-    }
-
-    public static void swap(int[] array, int ind1, int ind2) {
-        int tmp = array[ind1];
-        array[ind1] = array[ind2];
-        array[ind2] = tmp;
+        System.out.println(Arrays.toString(arr));
     }
 
 }
